3b81fb0161a90999e37139d0322fa7c24b28d06d
Rob Hess <hessro@oregonstate.edu>
2023-05-08 13:42:57 -0700

    Add create reservations endpoint.

--- api/reservations.js
+++ api/reservations.js
@@ -1,8 +1,15 @@
 const { Router } = require('express')
+const { getDb } = require("../lib/mongo")
+
 const router = Router()
 
-router.post('/', function (req, res, next) {
-    res.status(201).send({})
+router.post('/', async function (req, res, next) {
+    const db = getDb()
+    const collection = db.collection("reservations")
+    const result = await collection.insertOne(req.body)
+    res.status(201).send({
+        id: result.insertedId
+    })
 })
 
 module.exports = router
